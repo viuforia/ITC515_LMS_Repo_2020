075d6594897f041cda4676873cad9dfb88658e59
NiharikaGavvala <40655309+NiharikaGavvala@users.noreply.github.com>
2020-08-28 00:15:11 +1000

    LMS-NG-COM-59-changed enum and returnbook
    have commited the changes according to the code style guidelines.

--- src/library/returnBook/ReturnBookUI.java
+++ src/library/returnBook/ReturnBookUI.java
@@ -4,18 +4,27 @@ import java.util.Scanner;
 
 public class ReturnBookUI {
 
-	public static enum uI_sTaTe { INITIALISED, READY, INSPECTING, COMPLETED };
+	//public static enum uI_sTaTe { INITIALISED, READY, INSPECTING, COMPLETED };
+	public static enum uiState { INITIALISED, READY, INSPECTING, COMPLETED };  //changed uI_sTaTe to uiState
 
-	private rETURN_bOOK_cONTROL CoNtRoL;
-	private Scanner iNpUt;
-	private uI_sTaTe StATe;
+	//private rETURN_bOOK_cONTROL CoNtRoL;
+	private ReturnBookControl control;   //changed rETURN_bOOK_cONTROL to returnBookControl
+	//private Scanner iNpUt;
+	private Scanner input;  //changed iNpUt to input
+	//private uI_sTaTe StATe;
+	private UiState state; //changed uI_sTaTe to UiState and StATe to state
 
 	
-	public ReturnBookUI(rETURN_bOOK_cONTROL cOnTrOL) {
-		this.CoNtRoL = cOnTrOL;
-		iNpUt = new Scanner(System.in);
-		StATe = uI_sTaTe.INITIALISED;
-		cOnTrOL.sEt_uI(this);
+	//public ReturnBookUI(rETURN_bOOK_cONTROL cOnTrOL) {
+	public ReturnBookUI(returnBookControl control) {  //changed rETURN_bOOK_cONTROL to returnBookControl and cOnTrOL to control
+		//this.CoNtRoL = cOnTrOL;
+		this.control = control;  //changed CoNtRoL to control and cOnTrOL to control
+		//iNpUt = new Scanner(System.in);
+		input = new Scanner(System.in);  //changed iNpUt to input
+		//StATe = uI_sTaTe.INITIALISED;
+		state = UiState.INITIALISED;  //changed StATe to state and uI_sTaTe to UiState
+		//cOnTrOL.sEt_uI(this);
+		control.setUi(this);  //changed cOnTrOL to control and sEt_uI to setUi
 	}
 
 
